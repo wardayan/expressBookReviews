a286c0a60e004528d7612c50958d91e426d740f6
Ward Ayan <31561648+wardayan@users.noreply.github.com>
2024-12-30 10:50:11 +0300

    Modify to include promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,41 +36,97 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+let get_books = new Promise((resolve, reject) => {
+    try {
+        resolve(JSON.stringify(books, null, 4));
+    } catch (err) {
+        reject(err);
+    }
+});
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+    get_books.then(
+        (data) => res.send(data),
+        (err) => res.send("Error getting book information")
+    )
 });
+
+function get_book_ISBN (isbn) {
+    let promise = new Promise((resolve, reject) => {
+        try {
+            resolve(books[isbn]);
+        } catch (err) {
+            reject(err);
+        }
+    })
+
+    return promise;
+}
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    get_book_ISBN(isbn).then(
+        (data) => res.send(data),
+        (err) => res.send("Error getting book")
+    )
  });
   
+function get_book_author (author) {
+    let promise = new Promise((resolve, reject) => {
+        try {
+            let valid_books = {};
+            for (const [key, value] of Object.entries(books)) {
+                if (value.author === author) {
+                    valid_books[key] = value;            
+                } 
+            }
+            resolve(valid_books);
+
+        } catch (err) {
+            reject(err);
+        }
+    });
+
+    return promise;
+}
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let valid_books = {};
-    for (const [key, value] of Object.entries(books)) {
-        if (value.author === author) {
-            valid_books[key] = value;            
-        } 
-    }
-
-    res.send(valid_books);
+    get_book_author(author).then(
+        (data) => res.send(data),
+        (err) => res.send("Error getting book")
+    )
 });
+
+function get_book_title (title) {
+    let promise = new Promise((resolve, reject) => {
+        try {
+            let valid_books = {};
+            for (const [key, value] of Object.entries(books)) {
+                if (value.title === title) {
+                    valid_books[key] = value;            
+                } 
+            }
+            resolve(valid_books);
+        
+        } catch (err) {
+            reject(err);
+        }
+    });
+
+    return promise;
+}
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let valid_books = {};
-    for (const [key, value] of Object.entries(books)) {
-        if (value.title === title) {
-            valid_books[key] = value;            
-        } 
-    }
-
-    res.send(valid_books);
+    get_book_title(title).then(
+        (data) => res.send(data),
+        (err) => res.send("Error getting book")
+    )
 });
 
 //  Get book review
